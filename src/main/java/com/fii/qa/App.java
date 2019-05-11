b19e3a6ae099a346f9662354ac9328ca89ef3bc8
andreiherdes <andrei.herdes@info.uaic.ro>
2019-05-11 21:17:33 +0300

    GUI improvements

--- src/main/java/com/fii/qa/App.java
+++ src/main/java/com/fii/qa/App.java
@@ -1,23 +1,11 @@
 package com.fii.qa;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 
-import com.fii.qa.service.CRUDService;
-import com.fii.qa.service.DatabaseService;
-import com.fii.qa.service.TableService;
+import com.fii.qa.ui.DatabasePanel;
+import com.fii.qa.ui.TablePanel;
 
 /**
  * Hello world!
@@ -32,159 +20,55 @@ public class App extends JFrame {
 
 	// Name-constants to define the various dimensions
 	public static final int WINDOW_WIDTH = 500;
-	public static final int WINDOW_HEIGHT = 300;
+	public static final int WINDOW_HEIGHT = 500;
 
-	private DatabaseService databaseService = new DatabaseService();
-	private CRUDService crudService = new CRUDService();
-	private TableService tableService = new TableService();
-
-	private JButton createDbButton;
-	private JButton deleteDbButton;
-
-	private JTextField databaseName = new JTextField(20);
-	private JTextField columnNames = new JTextField(20);
-
-	private JButton createTableButton;
-	private JButton updateTableButton;
-	private JButton readTableButton;
-	private JButton deleteTableButton;
-
-	private JButton createEntryButton;
-	private JButton updateEntryButton;
-	private JButton readEntryButton;
-	private JButton deleteEntryButton;
-
-	private JTextField tableName = new JTextField(20);
-	private JTextField entryValue = new JTextField(20);
-
-	private JPanel databasePanel = new JPanel();
-	private JPanel crudPanel = new JPanel();
-	private JPanel fullPanel = new JPanel();
+	private DatabasePanel databasePanel = new DatabasePanel();
+	private TablePanel tablePanel = new TablePanel();
 
 	/** Constructor to setup the UI components */
 	public App() {
-
-		JLabel databaseNameLabel = new JLabel("Dabase name: ");
-		JLabel columnNamesLabel = new JLabel("Column names: ");
-
-		JPanel buttonPanel = new JPanel();
-		JPanel textFieldPanel = new JPanel();
-
-		createDbButton = new JButton("Create DB");
-		deleteDbButton = new JButton("Delete DB");
-
-		textFieldPanel.add(databaseNameLabel);
-		textFieldPanel.add(databaseName);
-		textFieldPanel.add(columnNamesLabel);
-		textFieldPanel.add(columnNames);
-
-		buttonPanel.add(createDbButton);
-		buttonPanel.add(deleteDbButton);
-
-		databasePanel.add(textFieldPanel, BorderLayout.CENTER);
-		databasePanel.add(buttonPanel, BorderLayout.PAGE_END);
-
-		fullPanel.add(databasePanel);
-
-		JLabel tableNameLabel = new JLabel("Table name: ");
-		JLabel columnLabel = new JLabel("Column name: ");
-		JLabel entryLabel = new JLabel("Entry value: ");
-
-		JPanel buttonPanelCRUD = new JPanel();
-		JPanel textFieldPanelCRUD = new JPanel();
-
-		textFieldPanelCRUD.add(tableNameLabel);
-		textFieldPanelCRUD.add(tableName);
-		textFieldPanelCRUD.add(entryLabel);
-		textFieldPanelCRUD.add(entryValue);
-		
-		createTableButton = new JButton("Create table");
-		updateTableButton = new JButton("Update table");
-		readTableButton = new JButton("Read table");
-		deleteTableButton = new JButton("Delete table");
-
-		deleteEntryButton = new JButton("Delete entry");
-		readEntryButton = new JButton("Read entry");
-		updateEntryButton = new JButton("Update entry");
-		createEntryButton = new JButton("Create entry");
-
-		buttonPanelCRUD.add(createTableButton);
-		buttonPanelCRUD.add(updateTableButton);
-		buttonPanelCRUD.add(readTableButton);
-		buttonPanelCRUD.add(deleteTableButton);
-
-		buttonPanelCRUD.add(deleteEntryButton);
-		buttonPanelCRUD.add(readEntryButton);
-		buttonPanelCRUD.add(updateEntryButton);
-		buttonPanelCRUD.add(createEntryButton);
-
-		crudPanel.add(textFieldPanelCRUD);
-		crudPanel.add(buttonPanelCRUD, BorderLayout.PAGE_END);
-
-		fullPanel.add(crudPanel, BorderLayout.PAGE_END);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit when close button clicked
 		setTitle("DB Administrator");
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setVisible(true); // show it
 
-		this.add(fullPanel);
+		JTabbedPane tp = new JTabbedPane();
+		tp.setBounds(50, 50, 200, 200);
+
+		tp.add("Database", databasePanel);
+		tp.add("Table", tablePanel);
+		this.add(tp);
 		this.pack();
 
-		createDbButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				databaseService.createDatabase(databaseName.getText());
-			}
-		});
-
-		deleteDbButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				databaseService.deleteDatabase(databaseName.getText());
-			}
-		});
-
-		createTableButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String[] columnNamesArray = columnNames.getText().split(",");
-
-				tableService.createTable(databaseName.getText(), tableName.getText(), Arrays.asList(columnNamesArray));
-			}
-		});
-
-		createEntryButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String[] entryNamesArray = entryValue.getText().split(",");
-				String[] columnNamesArray = columnNames.getText().split(",");
-
-				Map<String, String> values = IntStream.range(0, columnNamesArray.length).boxed()
-						.collect(Collectors.toMap(i -> columnNamesArray[i], i -> entryNamesArray[i]));
-
-				crudService.insertRow(databaseName.getText(), tableName.getText(), values);
-
-			}
-		});
-
-		deleteEntryButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String[] entryNamesArray = entryValue.getText().split(",");
-				String[] columnNamesArray = columnNames.getText().split(",");
-
-				Map<String, String> values = IntStream.range(0, columnNamesArray.length).boxed()
-						.collect(Collectors.toMap(i -> columnNamesArray[i], i -> entryNamesArray[i]));
-				crudService.deleteRow(databaseName.getText(), tableName.getText(), values);
-
-			}
-		});
+		/*
+		 * createEntryButton.addActionListener(new ActionListener() {
+		 * 
+		 * @Override public void actionPerformed(ActionEvent e) { String[]
+		 * entryNamesArray = entryValue.getText().split(","); String[] columnNamesArray
+		 * = columnNames.getText().split(",");
+		 * 
+		 * Map<String, String> values = IntStream.range(0,
+		 * columnNamesArray.length).boxed() .collect(Collectors.toMap(i ->
+		 * columnNamesArray[i], i -> entryNamesArray[i]));
+		 * 
+		 * crudService.insertRow(databaseName.getText(), tableName.getText(), values);
+		 * 
+		 * } });
+		 * 
+		 * deleteEntryButton.addActionListener(new ActionListener() {
+		 * 
+		 * @Override public void actionPerformed(ActionEvent e) { String[]
+		 * entryNamesArray = entryValue.getText().split(","); String[] columnNamesArray
+		 * = columnNames.getText().split(",");
+		 * 
+		 * Map<String, String> values = IntStream.range(0,
+		 * columnNamesArray.length).boxed() .collect(Collectors.toMap(i ->
+		 * columnNamesArray[i], i -> entryNamesArray[i]));
+		 * crudService.deleteRow(databaseName.getText(), tableName.getText(), values);
+		 * 
+		 * } });
+		 */
 	}
 
 	/** The entry main() method */
--- src/main/java/com/fii/qa/service/DatabaseService.java
+++ src/main/java/com/fii/qa/service/DatabaseService.java
@@ -1,87 +1,104 @@
 package com.fii.qa.service;
-import org.apache.commons.lang3.ObjectUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 public class DatabaseService {
-    private static final String DATABASE = "database";
-    private static final String databasesDirectory ="databases/";
+	private static final String DATABASE = "database";
+	private static final String databasesDirectory = "databases/";
 
-    public boolean createDatabase(String name) {
-        final String databaseFilePath = databasesDirectory + name + ".xml";
+	public boolean createDatabase(String name) {
+		final String databaseFilePath = databasesDirectory + name + ".xml";
 
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.newDocument();
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
 
-            Element root = doc.createElement(DATABASE);
-            root.setAttribute("name", name);
-            doc.appendChild(root);
+			Element root = doc.createElement(DATABASE);
+			root.setAttribute("name", name);
+			doc.appendChild(root);
 
-            root.appendChild(doc.createElement("tables"));
+			root.appendChild(doc.createElement("tables"));
 
-            XmlWriterService.writeXMLFile(doc, databaseFilePath);
-            return true;
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
+			XmlWriterService.writeXMLFile(doc, databaseFilePath);
+			return true;
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
-    public void deleteDatabase(String name) {
-        final String databaseFilePath = databasesDirectory + name + ".xml";
+	public void deleteDatabase(String name) {
+		final String databaseFilePath = databasesDirectory + name + ".xml";
 
-        if(!databaseExists(name)) {
-            System.out.println("Database does not exist!");
-            return;
-        }
+		if (!databaseExists(name)) {
+			System.out.println("Database does not exist!");
+			return;
+		}
 
-        File xmlFile = new File(databaseFilePath);
-        xmlFile.delete();
-    }
+		File xmlFile = new File(databaseFilePath);
+		xmlFile.delete();
+	}
 
-    public void changeDatabase(String name, String newName) {
-        final String databaseFilePath = databasesDirectory + name + ".xml";
+	public void changeDatabase(String name, String newName) {
+		final String databaseFilePath = databasesDirectory + name + ".xml";
 
-        if(!databaseExists(name)) {
-            System.out.println("Database does not exist!");
-            return;
-        }
+		if (!databaseExists(name)) {
+			System.out.println("Database does not exist!");
+			return;
+		}
 
-        if(name.equals(newName)) {
-            return;
-        }
+		if (name.equals(newName)) {
+			return;
+		}
 
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(new File(databaseFilePath));
-            doc.getDocumentElement().normalize();
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new File(databaseFilePath));
+			doc.getDocumentElement().normalize();
 
-            Element database = (Element) doc.getElementsByTagName(DATABASE).item(0);
-            database.setAttribute("name", newName);
-            XmlWriterService.writeXMLFile(doc, databaseFilePath);
+			Element database = (Element) doc.getElementsByTagName(DATABASE).item(0);
+			database.setAttribute("name", newName);
+			XmlWriterService.writeXMLFile(doc, databaseFilePath);
 
-            File file = new File(databaseFilePath);
-            file.renameTo(new File(databasesDirectory + newName + ".xml"));
+			File file = new File(databaseFilePath);
+			file.renameTo(new File(databasesDirectory + newName + ".xml"));
 
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private boolean databaseExists(String name) {
-        final String databaseFilePath = databasesDirectory + name + ".xml";
+	public String[] getAllDatabases() {
+		File[] files = new File(databasesDirectory).listFiles();
+		String[] databaseNames = new String[files.length];
 
-        return new File(databaseFilePath).exists();
+		for (File file : files) {
+			int counter = 0;
+			if (file.isFile()) {
+				databaseNames[counter] = file.getName().substring(0, file.getName().indexOf("."));
+				counter++;
+			}
+		}
 
-    }
+		return databaseNames;
+	}
+
+	private boolean databaseExists(String name) {
+		final String databaseFilePath = databasesDirectory + name + ".xml";
+
+		return new File(databaseFilePath).exists();
+
+	}
 
 }
